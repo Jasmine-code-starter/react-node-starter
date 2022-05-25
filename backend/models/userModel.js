a95f609e6e2e36742d552eaa8deabd9050029b72
huangyuanzhen <1356409766@qq.com>
2022-05-25 09:12:08 +0800

    back: add update update user & users api

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,16 +1,32 @@
 
-import { Sequelize, DataTypes } from 'sequelize';
+import { DataTypes } from 'sequelize';
 import sequelize from '../db/index.js';
 
 const User = sequelize.define('User', {
     username: {
-        type: Sequelize.STRING,
+        type: DataTypes.STRING,
         allowNull: false,
     },
     password: {
-        type: Sequelize.STRING,
+        type: DataTypes.STRING,
         allowNull: false,
     },
+    firstname: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    }, 
+    lastname: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    }, 
+    gender: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+    }, 
+    birthdate: {
+        type: DataTypes.DATE,
+        allowNull: true,
+    }
   }, {
       tableName: 'User',
       timestamps: false,
--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -10,10 +10,10 @@ userRouter.post('/register', expressAsyncHandler(async (req, res) => {
     const userInfo = req.body;
 
     if (!userInfo.username || !userInfo.password) {
-        return res.send({status: 1, message: '输入不合法'});
+        return res.send({ status: 1, message: '输入不合法' });
     }
 
-    const findUser = await User.findOne({where: {username: userInfo.username}});
+    const findUser = await User.findOne({ where: { username: userInfo.username } });
     if (findUser) {
         return res.errorHanler('用户名被占用，请更换用户名');
     }
@@ -24,32 +24,65 @@ userRouter.post('/register', expressAsyncHandler(async (req, res) => {
     });
 
     if (!createUser) {
-        return res.errorHanler('注册失败，请稍后再试');
+        return res.errorHanler('Register failed, Please try again!');
     }
-    res.send({status: 0, message: '注册成功' });
-   
+    res.send({ status: 0, message: 'Register success' });
+
 }))
 
 
 userRouter.post(
-    '/login', 
+    '/login',
     expressAsyncHandler(async (req, res) => {
-        const userInfo  = req.body;
+        const userInfo = req.body;
         if (!userInfo.username || !userInfo.password) {
-            return res.send({status: 1, message: '输入不合法'});
+            return res.send({ status: 1, message: 'Invalid username or password !' });
         }
 
         const findUser = await User.findOne({ where: { username: userInfo.username } });
         if (findUser) {
             if (bcrypt.compareSync(userInfo.password, findUser.password)) {
-                return res.send({status: 0, message: '登录成功!', token: 'Bearer' + generateToken(userInfo)});
+                return res.send({ status: 0, message: '登录成功!', token: 'Bearer' + generateToken(userInfo) });
             } else {
-                return  res.status(401).send({ message: "Invalid email or password" });
+                return res.status(401).send({ message: "Invalid email or password" });
             }
         }
 
-        res.errorHanler('用户不存在，请先进行注册！');
+        res.errorHanler('User is not exist，Please register first！');
+
+    }));
+
+
+userRouter.put(
+    '/update/:id',
+    expressAsyncHandler(async (req, res) => {
+        console.log(req.params.id);
+        let user = await User.findByPk(req.params.id);
+        if (user) {
+            user.username = req.body.username || user.username;
+            const { firstname, lastname, gender, birthdate } = req.body;
+            await user.set({
+                firstname,
+                lastname,
+                gender,
+                birthdate
+            })
+            const updateUser = await user.save();
+
+            res.send({ status: 0, message: 'update user success!', token: 'Bearer' + generateToken(updateUser), user: updateUser })
+        } else {
+            return res.errorHanler('update failed');
+        }
 
 }));
+
+userRouter.get(
+    '/',
+    expressAsyncHandler(async (req, res) => {
+        console.log(req.params.id);
+        const users = await User.findAll();
+        res.send(users);
+}));
+
 
 export default userRouter;
